f8122568486e8b9872576272c8b6164efe762238
hasnain218 <muazzamh504@gmail.com>
2025-02-18 15:38:59 +0000

    ch#4

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,3 +1,38 @@
-let score = 33;
+// let score = 33;
 
-console.log(typeof score);
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// let isLoggedIn = 1;
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// let someNumber = 33;
+// let stringNumber = String(someNumber)
+// console.log(typeof stringNumber)
+// console.log(stringNumber);
+
+
+// Operation
+
+
+let value = 3
+let negValue = -value;
+// console.log(negValue);
+
+let str1 = "hello"
+let str2 = " Developer"
+let str3 = str1 + str2
+
+// console.log(str3);
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter);
+
+
